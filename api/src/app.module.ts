a30156a69ebfdef50cebdcf37983e96b4ebd60c2
DefoortMerijn <merijn.defoort@student.howest.be>
2022-09-27 12:36:15 +0200

    feat: GraphQL setup

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { graphqlConfig } from './bootstrap/graphqlConfig'
+import { typeORMConfig } from './bootstrap/typeORMConfig'
+import { ApolloDriverConfig } from '@nestjs/apollo/dist/interfaces'
+
+//TODO: create birds module
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    GraphQLModule.forRoot<ApolloDriverConfig>(graphqlConfig),
+  ], //Enchancment? move to async provider
   controllers: [],
   providers: [],
 })
