9974ed58df295e0b926b3ea0140b802d4b774cee
DefoortMerijn <merijn.defoort@student.howest.be>
2022-11-14 14:10:05 +0100

    linting fix

--- pwa/src/components/generic/AppHeader.vue
+++ pwa/src/components/generic/AppHeader.vue
@@ -1,21 +1,20 @@
 <template>
-  <header class=" bg-neutral-100 px-6 py-6">
-    <nav class="max-w-6xl mx-auto flex items-center justify-between">
-      <router-link to="/" class="focus-visible:ring-2 outline-none rounded-md">
-        <Logo class="h-12 w-12 justify-start " />
+  <header class="bg-neutral-100 px-6 py-6">
+    <nav class="mx-auto flex max-w-6xl items-center justify-between">
+      <router-link to="/" class="rounded-md outline-none focus-visible:ring-2">
+        <Logo class="h-12 w-12 justify-start" />
       </router-link>
-      <AppNav class="hidden sm:flex"/>
+      <AppNav class="hidden sm:flex" />
     </nav>
   </header>
 </template>
 
 <script lang="ts">
 import Logo from './Logo.vue'
-import AppNav from './AppNav.vue';
+import AppNav from './AppNav.vue'
 
 export default {
-  setup() {
-  },
+  setup() {},
   components: { Logo, AppNav },
 }
 </script>
--- pwa/src/components/generic/AppNav.vue
+++ pwa/src/components/generic/AppNav.vue
@@ -3,28 +3,49 @@
     class="grid w-full grid-cols-4 items-center text-center text-xs font-medium sm:w-auto sm:space-x-3 sm:text-left sm:text-sm"
   >
     <li>
-      <router-link exact-active-class="text-cyan-500" to="/" class="px-3 py-6 focus-visible:ring-2 outline-none inline-block rounded-md">      <Home class="h-5 mx-auto mb-2 sm:hidden" />
- Home </router-link>
+      <router-link
+        exact-active-class="text-cyan-500"
+        to="/"
+        class="inline-block rounded-md px-3 py-6 outline-none focus-visible:ring-2"
+      >
+        <Home class="mx-auto mb-2 h-5 sm:hidden" /> Home
+      </router-link>
     </li>
     <li>
-      <router-link active-class="text-cyan-500" to="/birds" class="px-3 py-6 focus-visible:ring-2 outline-none inline-block rounded-md">      <Feather class="h-5 mx-auto mb-2 sm:hidden" />
- Birds </router-link>
+      <router-link
+        active-class="text-cyan-500"
+        to="/birds"
+        class="inline-block rounded-md px-3 py-6 outline-none focus-visible:ring-2"
+      >
+        <Feather class="mx-auto mb-2 h-5 sm:hidden" /> Birds
+      </router-link>
     </li>
     <li>
-      <router-link active-class="text-cyan-500" to="/observations" class="px-3 py-6 focus-visible:ring-2 outline-none inline-block rounded-md">
-        <Scroll class="h-5 mx-auto mb-2 sm:hidden" />
-Observations
+      <router-link
+        active-class="text-cyan-500"
+        to="/observations"
+        class="inline-block rounded-md px-3 py-6 outline-none focus-visible:ring-2"
+      >
+        <Scroll class="mx-auto mb-2 h-5 sm:hidden" />
+        Observations
       </router-link>
     </li>
     <li class="sm:hidden">
-      
-
-      <router-link active-class="text-cyan-500" to="/account" class="px-3 py-6 focus-visible:ring-2 outline-none inline-block rounded-md">
-        <User class="h-5 mx-auto mb-2 sm:hidden" /> User </router-link>
+      <router-link
+        active-class="text-cyan-500"
+        to="/account"
+        class="inline-block rounded-md px-3 py-6 outline-none focus-visible:ring-2"
+      >
+        <User class="mx-auto mb-2 h-5 sm:hidden" /> User
+      </router-link>
     </li>
 
-    <li class="pl-6 sm:block hidden">
-      <router-link active-class="text-cyan-500" to="/account" class="px-3 py-6 focus-visible:ring-2 outline-none inline-block rounded-md">
+    <li class="hidden pl-6 sm:block">
+      <router-link
+        active-class="text-cyan-500"
+        to="/account"
+        class="inline-block rounded-md px-3 py-6 outline-none focus-visible:ring-2"
+      >
         {{ user ? user?.displayName : 'Login' }}
       </router-link>
     </li>
@@ -42,6 +63,6 @@ export default {
       user,
     }
   },
-  components: { Home,Feather, Scroll, Clipboard, User },
+  components: { Home, Feather, Scroll, Clipboard, User },
 }
 </script>
